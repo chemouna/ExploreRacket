14fb6c7fb3dddcce95e8d0d902d39b4847df3047
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-04 08:01:19 +0200

    Add unzip definition.

--- explore.rkt
+++ explore.rkt
@@ -2,3 +2,15 @@
 (for/list ([x '(1 2 3)]
            #:when (odd? x))
    (+ x 1))
+
+;; unzip in racket (Continuation passing style)
+(define (unzip/callback lst k)
+  (match lst
+    ['() (k '() '())]
+    [(cons (list a b) tl)
+       (unzip/callback tl (λ (as bs)
+                    (k (cons a as) (cons b bs))))]))
+
+
+(unzip/callback '((1 2) (3 4) (5 6)) (λ (as bs)
+  as))
